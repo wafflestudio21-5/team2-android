264cc60d743879a32c3ef589e03fcc57ed00a684
Glenn-syj <glenn.syj@gmail.com>
2024-01-02 23:40:12 +0900

    [TSK-67] Feat: ViewModel 의존성 주입
    앞선 커밋에서 놓친 ViewModel에 의존성 주입을 적용했습니다.

--- app/src/main/java/com/wafflestudio/bunnybunny/viewModel/MainViewModel.kt
+++ app/src/main/java/com/wafflestudio/bunnybunny/viewModel/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.wafflestudio.bunnybunny.viewModel
 
-class MainViewModel {
-}
+import androidx.lifecycle.ViewModel
+import com.wafflestudio.bunnybunny.data.example.ExampleRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val repository: ExampleRepository
+): ViewModel()
