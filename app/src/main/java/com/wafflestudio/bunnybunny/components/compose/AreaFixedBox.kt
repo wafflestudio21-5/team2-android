a6ee4d94cd33be74a4a177e1d975e448c97aea68
Glenn-syj <glenn.syj@gmail.com>
2024-02-03 03:37:19 +0900

    [TSK-137] Feat: 지역 선택 화면 구현 및 지역 선택 드롭다운
    - 오류 개선 필요: API 연동, 지역  변화 시 물품 목록 불러오기, UI 화면
    - 드롭다운 메뉴 구현
    - 지역 설정 화면 내 간단한 버튼 및 슬라이더 생성

--- app/src/main/java/com/wafflestudio/bunnybunny/components/compose/AreaFixedBox.kt
+++ app/src/main/java/com/wafflestudio/bunnybunny/components/compose/AreaFixedBox.kt
@@ -27,37 +27,33 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.wafflestudio.bunnybunny.data.example.RefAreaId
 import com.wafflestudio.bunnybunny.data.example.SimpleAreaData
-import com.wafflestudio.bunnybunny.pages.RefArea
+import com.wafflestudio.bunnybunny.viewModel.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @Composable
-fun AreaBox(modifier: Modifier, areaDetail: SimpleAreaData, areaIds: MutableList<RefArea>) {
+fun AreaFixedBox(modifier: Modifier, areaDetail: SimpleAreaData, areaIds: MutableList<Int>, navController: NavController) {
+
     val areaId = areaDetail.id
     val context = LocalContext.current
     var isClicked by remember { mutableStateOf(false) }
+    val viewModel: MainViewModel = hiltViewModel()
+
 
     Box(
         modifier = modifier
-            .background(if ((isClicked)&&(areaIds.map { it.id }.contains(areaId))) Color.LightGray else Color.White)
+            .background(if ((isClicked) && (areaIds.contains(areaId))) Color.Gray else Color.White)
             .clickable {
-                if ((!isClicked)&&(areaIds.size == 2)) {
-                    CoroutineScope(Dispatchers.Main).launch() {
-                        Toast.makeText(context, "최대 2개의 지역만 선택 가능합니다.", Toast.LENGTH_SHORT).show()
-                    }
-                } else if ((isClicked)&&(areaIds.map { it.id }.contains(areaId))) {
-                    areaIds.removeIf { it.id == areaId }
-                    isClicked = !isClicked
-                } else if (!areaIds.map { it.id }.contains(areaId)) {
-                    areaIds.add(RefArea(areaId, areaDetail.name))
-                    isClicked = !isClicked
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.addRefAreaId(areaId)
+                    navController.popBackStack()
                 }
-                Log.d("AreaBox", "$areaIds")
             }
     ) {
         Row(
@@ -76,12 +72,4 @@ fun AreaBox(modifier: Modifier, areaDetail: SimpleAreaData, areaIds: MutableList
             )
         }
     }
-}
-
-fun DrawScope.drawBorderBottom(size: Size, thickness: Dp, color: Color) {
-    drawRect(
-        color = color,
-        topLeft = Offset(0f, size.height - thickness.toPx()),
-        size = Size(size.width, thickness.toPx())
-    )
 }
--- app/src/main/java/com/wafflestudio/bunnybunny/pages/AreaChangePage.kt
+++ app/src/main/java/com/wafflestudio/bunnybunny/pages/AreaChangePage.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -22,7 +23,6 @@ import com.wafflestudio.bunnybunny.components.compose.AreaContents
 import com.wafflestudio.bunnybunny.components.compose.AreaSearchBar
 import com.wafflestudio.bunnybunny.components.compose.BasicButton
 import com.wafflestudio.bunnybunny.data.example.SignupRequest
-import com.wafflestudio.bunnybunny.data.example.SocialSignupRequest
 import com.wafflestudio.bunnybunny.utils.mutableListToString
 import com.wafflestudio.bunnybunny.utils.stringToMutableList
 import com.wafflestudio.bunnybunny.viewModel.MainViewModel
@@ -33,39 +33,15 @@ import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
 @Composable
-fun SocialAreaChoosePage(nickname: String, idToken: String, navController: NavController, onNavigateToSignIn: () -> Unit) {
-    val refAreaIds = mutableListOf<RefArea>()
+fun AreaChangePage(viewModel: MainViewModel, navController: NavController) {
+    val refAreaIds = remember { mutableStateListOf<RefArea>() }
     val viewModel = hiltViewModel<MainViewModel>()
-    val areaDetails by viewModel.areaDetails.collectAsState()
     val context = LocalContext.current;
-
-//    val updateRefAreaNames: (List<String>) -> Unit = { updatedList ->
-//        refAreaNames = updatedList.toMutableList()
-//        Log.d("AreaChoosePage", "Updated refAreaNames: $refAreaNames")
-//    }
+    val areaDetails by viewModel.areaDetails.collectAsState()
 
     Column {
         AreaSearchBar(modifier = Modifier, areaDetails)
-        AreaContents(modifier = Modifier, refAreaIds, areaDetails, navController, false)
-        BasicButton(modifier = Modifier, onClick = {
-            Log.d("ACP", refAreaIds.toString())
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    val response = viewModel.trySocialSignUp(SocialSignupRequest(nickname, "profile.com", refAreaIds.map { it.id }, idToken))
-                    Log.d("ACP", "success: ${response}")
-                    withContext(Dispatchers.Main) {
-
-                    }
-                }
-                catch (e: HttpException){
-                    val message = e.response()?.errorBody()?.toString()
-                    Log.d("ACP", "error: $message")
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }, text = "회원가입!", networkBoolean = false)
+        AreaContents(modifier = Modifier, refAreaIds, areaDetails, navController, true)
     }
 
 
